3ac863a1435c10f05dafabcc04592230bd4523ef
Ethan Wallace <ethan.wallace.91@gmail.com>
2024-12-13 19:50:03 -0500

    tabs and copy buttons

--- packages/web/src/components/element-display/element-display.tsx
+++ packages/web/src/components/element-display/element-display.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, Watch, Element, State } from '@stencil/core';
+import { Component, Host, h, Prop, Watch, Element, State, Fragment } from '@stencil/core';
 import Prism from 'prismjs';
 import prettier from 'prettier';
 import prettierPluginHTML from 'prettier/plugins/html';
@@ -26,7 +26,7 @@ export type EventType = {
 
 @Component({
   tag: 'element-display',
-  styleUrls: ['element-display.css', 'prism.css'],
+  styleUrls: ['prism.css', 'element-display.css'],
   shadow: true,
 })
 export class ElementDisplay {
@@ -35,6 +35,8 @@ export class ElementDisplay {
   private displayElement?: Element;
   private htmlCodePreview?: HTMLElement;
   private reactCodePreview?: HTMLElement;
+  private copyHTMLButton?: HTMLElement;
+  private copyReactButton?: HTMLElement;
 
   private attributeObject;
   private slotObject;
@@ -71,6 +73,11 @@ export class ElementDisplay {
   }
 
   @State() display: string = 'attr';
+  @State() showCode: boolean = true;
+
+  private setDisplay(str) {
+    this.display = str;
+  }
 
   /////// Attribute changes
 
@@ -142,6 +149,24 @@ export class ElementDisplay {
     );
   }
 
+  private copyCode(e) {
+    let code = '';
+    if (e.target.name === 'html') {
+      code = this.htmlCodePreview.textContent;
+      this.copyHTMLButton.textContent = 'Code copied';
+      setTimeout(() => {
+        this.copyHTMLButton.textContent = 'Copy HTML';
+      }, 3000);
+    } else {
+      code = this.reactCodePreview.textContent;
+      this.copyReactButton.textContent = 'Code copied';
+      setTimeout(() => {
+        this.copyReactButton.textContent = 'Copy React';
+      }, 3000);
+    }
+    navigator.clipboard.writeText(code);
+  }
+
   async componentWillLoad() {
     this.validateAttrs();
     this.validateSlots();
@@ -162,136 +187,224 @@ export class ElementDisplay {
         </div>
 
         <div class="code-frame">
-          <pre class="language-html">
-            <code
-              id="html"
-              ref={element => (this.htmlCodePreview = element as HTMLElement)}
-            ></code>
-          </pre>
-          <pre class="language-html">
-            <code
-              id="react"
-              ref={element => (this.reactCodePreview = element as HTMLElement)}
-            ></code>
-          </pre>
+          <div class="code-actions">
+            <gcds-button
+              button-role='secondary'
+              onClick={() => { this.showCode = !this.showCode }}
+              >
+                {this.showCode ? 'Hide code' : 'Show code'}
+              </gcds-button>
+
+            {this.showCode && (
+              <>
+                <gcds-button
+                  button-role='secondary'
+                  name='html'
+                  onClick={(e) => { this.copyCode(e) }}
+                  ref={element => (this.copyHTMLButton = element as HTMLElement)}
+                >
+                  Copy HTML
+                </gcds-button>
+                <gcds-button
+                  button-role='secondary'
+                  name='react'
+                  onClick={(e) => { this.copyCode(e) }}
+                  ref={element => (this.copyReactButton = element as HTMLElement)}
+                >
+                  Copy React
+                </gcds-button>
+              </>
+            )}
+          </div>
+          <div class={`code-preview${!this.showCode && ' hidden'}`}>
+            <pre class="language-html">
+              <code
+                id="html"
+                ref={element => (this.htmlCodePreview = element as HTMLElement)}
+              ></code>
+            </pre>
+            <pre class="language-html">
+              <code
+                id="react"
+                ref={element => (this.reactCodePreview = element as HTMLElement)}
+              ></code>
+            </pre>
+          </div>
         </div>
 
-        <table class="attributes">
-          <tr>
-            <th>Attributes</th>
-            <th>Type</th>
-            <th>Default value</th>
-            <th>Control</th>
-          </tr>
-          {this.attributeObject &&
-            this.attributeObject.map(attr => {
-              let control = '';
+        <div>
+          <div role="tablist">
+            <gcds-button
+              id="attributes"
+              button-role="secondary"
+              role="tab"
+              onClick={() => this.setDisplay('attr')}
+              aria-selected={this.display === 'attrs' ? 'true' : 'false'}
+            >
+              Attributes & properties
+            </gcds-button>
+            {this.slotObject && (
+              <gcds-button
+                id="slots"
+                button-role="secondary"
+                role="tab"
+                onClick={() => this.setDisplay('slots')}
+                aria-selected={this.display === 'slots' ? 'true' : 'false'}
+              >
+                Slots
+              </gcds-button>
+            )}
+            {this.eventObject && (
+              <gcds-button
+                id="events"
+                button-role="secondary"
+                role="tab"
+                onClick={() => this.setDisplay('events')}
+                aria-selected={this.display === 'events' ? 'true' : 'false'}
+              >
+                Events
+              </gcds-button>
+            )}
+          </div>
 
-              let displayValue =
-                this.displayElement.getAttribute(attr.name) != null
-                  ? this.displayElement.getAttribute(attr.name)
-                  : attr?.defaultValue;
+          <div
+            role="tabpanel"
+            aria-labbeledby="attributes"
+            tabindex="0"
+            class={this.display != 'attr' && 'hidden'}
+          >
+            <table class="attributes">
+              <tr>
+                <th>Attributes</th>
+                <th>Type</th>
+                <th>Default value</th>
+                <th>Control</th>
+              </tr>
+              {this.attributeObject &&
+                this.attributeObject.map(attr => {
+                  let control = '';
 
-              if (attr.control === 'select') {
-                const options = JSON.parse(attr.options);
-                // console.log(options);
-                control = (
-                  <gcds-select
-                    label={attr.name}
-                    selectId={attr.name}
-                    name={attr.name}
-                    value={displayValue}
-                    onInput={e => this.handleAttrInput(e)}
-                    onChange={e => this.handleAttrInput(e)}
-                  >
-                    {options.map(option => (
-                      <option key={option} value={option}>
-                        {option}
-                      </option>
-                    ))}
-                  </gcds-select>
-                );
-              } else if (attr.control === 'text') {
-                control = (
-                  <gcds-input
-                    name={attr.name}
-                    label={attr.name}
-                    inputId={attr.name}
-                    hide-label
-                    type="text"
-                    value={displayValue}
-                    onInput={e => this.handleAttrInput(e)}
-                    onChange={e => this.handleAttrInput(e)}
-                  ></gcds-input>
-                );
-              }
+                  let displayValue =
+                    this.displayElement.getAttribute(attr.name) != null
+                      ? this.displayElement.getAttribute(attr.name)
+                      : attr?.defaultValue;
 
-              return (
+                  if (attr.control === 'select') {
+                    const options = JSON.parse(attr.options);
+                    // console.log(options);
+                    control = (
+                      <gcds-select
+                        label={attr.name}
+                        selectId={attr.name}
+                        name={attr.name}
+                        value={displayValue}
+                        onInput={e => this.handleAttrInput(e)}
+                        onChange={e => this.handleAttrInput(e)}
+                      >
+                        {options.map(option => (
+                          <option key={option} value={option}>
+                            {option}
+                          </option>
+                        ))}
+                      </gcds-select>
+                    );
+                  } else if (attr.control === 'text') {
+                    control = (
+                      <gcds-input
+                        name={attr.name}
+                        label={attr.name}
+                        inputId={attr.name}
+                        hide-label
+                        type="text"
+                        value={displayValue}
+                        onInput={e => this.handleAttrInput(e)}
+                        onChange={e => this.handleAttrInput(e)}
+                      ></gcds-input>
+                    );
+                  }
+
+                  return (
+                    <tr>
+                      <td>{attr.name}</td>
+                      <td>{attr.type}</td>
+                      <td>{attr?.defaultValue}</td>
+                      <td>{control}</td>
+                    </tr>
+                  );
+                })}
+            </table>
+          </div>
+
+          {this.slotObject && (
+            <div
+              role="tabpanel"
+              aria-labbeledby="slots"
+              tabindex="0"
+              class={this.display != 'slots' && 'hidden'}
+            >
+              <table class="slots">
+                <caption>
+                  Slots allow passing text or patterns to the component.
+                </caption>
                 <tr>
-                  <td>{attr.name}</td>
-                  <td>{attr.type}</td>
-                  <td>{attr?.defaultValue}</td>
-                  <td>{control}</td>
+                  <th>Slot name</th>
+                  <th>Description</th>
+                  <th>Control</th>
                 </tr>
-              );
-            })}
-        </table>
 
-        {this.slotObject && (
-          <table class="slots">
-            <caption>
-              Slots allow passing text or patterns to the component.
-            </caption>
-            <tr>
-              <th>Slot name</th>
-              <th>Description</th>
-              <th>Control</th>
-            </tr>
+                {this.slotObject.map(slot => {
+                  const controlValue = this.getSlotValue(slot.name);
+                  const control = (
+                    <gcds-textarea
+                      label={slot.name}
+                      textareaId={slot.name}
+                      name={slot.name}
+                      hideLabel
+                      value={controlValue}
+                      onInput={e => this.handleSlotInput(e)}
+                      onChange={e => this.handleSlotInput(e)}
+                    ></gcds-textarea>
+                  );
+                  return (
+                    <tr>
+                      <td>{slot.name}</td>
+                      <td>{slot.description}</td>
+                      <td>{control}</td>
+                    </tr>
+                  );
+                })}
+              </table>
+            </div>
+          )}
 
-            {this.slotObject.map(slot => {
-              const controlValue = this.getSlotValue(slot.name);
-              const control = (
-                <gcds-textarea
-                  label={slot.name}
-                  textareaId={slot.name}
-                  name={slot.name}
-                  hideLabel
-                  value={controlValue}
-                  onInput={e => this.handleSlotInput(e)}
-                  onChange={e => this.handleSlotInput(e)}
-                ></gcds-textarea>
-              );
-              return (
+          {this.eventObject && (
+            <div
+              role="tabpanel"
+              aria-labbeledby="events"
+              tabindex="0"
+              class={this.display != 'events' && 'hidden'}
+            >
+              <table class="events">
+                <caption>Custom events the component has</caption>
                 <tr>
-                  <td>{slot.name}</td>
-                  <td>{slot.description}</td>
-                  <td>{control}</td>
+                  <th>Event name</th>
+                  <th>Description</th>
+                  <th>Details</th>
                 </tr>
-              );
-            })}
-          </table>
-        )}
 
-        {this.eventObject && (
-          <table class="events">
-            <caption>Custom events the component has</caption>
-            <tr>
-              <th>Event name</th>
-              <th>Description</th>
-              <th>Details</th>
-            </tr>
-
-            {this.eventObject.map(event => {
-              return (
-                <tr>
-                  <td>{event.name}</td>
-                  <td>{event.description}</td>
-                  <td>{event.details}</td>
-                </tr>
-              );
-            })}
-          </table>
-        )}
+                {this.eventObject.map(event => {
+                  return (
+                    <tr>
+                      <td>{event.name}</td>
+                      <td>{event.description}</td>
+                      <td>{event.details}</td>
+                    </tr>
+                  );
+                })}
+              </table>
+            </div>
+          )}
+        </div>
       </Host>
     );
   }
